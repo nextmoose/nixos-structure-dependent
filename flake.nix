724555b4794b06dbe3bd5a193e6d5e8eebcf755c
Emory Merryman <emory.merryman@gmail.com>
2022-10-21 13:08:39 -0400

    simplify

--- flake.nix
+++ flake.nix
@@ -14,13 +14,7 @@
 		    "bin"
 		    ''
 		      WORK_DIR=$( ${ pkgs.mktemp } --directory ) &&
-		        cleanup ( )
-		          {
-			    ${ pkgs.find }/bin/find ${ nixos-structure-utils.dollar "WORK_DIR" } -type f -exec ${ pkgs.coreutils }/bin/shred --force --remove=wipesync {} \; &&
-			      rm --recursive --force ${ nixos-structure-utils.dollar "WORK_DIR" } &&
-			      cd $( ${ pkgs.mktemp }/bin/mktemp --directory ${ nixos-structure-utils.dollar "WORK_DIR" }/XXXXXXXX )
-		          } &&
-			trap CLEANUP exit
+		        ${ pkgs.coreutils }/bin/true
 		    '' ;
             }
       ) ;
