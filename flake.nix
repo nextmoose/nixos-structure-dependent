e1dc0112ddbe603a91ad09100503f6c733cc5b0c
Emory Merryman <emory.merryman@gmail.com>
2022-10-21 14:20:56 -0400

    simplify

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
-  inputs = { nixpkgs.url = "github:nixos/nixpkgs" ; flake-utils.url = "github:numtide/flake-utils" ; } ;
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs" ; flake-utils.url = "github:numtide/flake-utils" ; nixos-structure-utils.url = "github:nextmoose/nixos-structure-utils" ; } ;
   outputs =
-    { self , nixpkgs , flake-utils } :
+    { self , nixpkgs , flake-utils , nixos-structure-utils } :
       flake-utils.lib.eachDefaultSystem
       (
         system :
