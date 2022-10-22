ec9bceaff70a0b7620bc6d78b1a89aa3ea742fd7
Emory Merryman <emory.merryman@gmail.com>
2022-10-21 22:35:54 -0400

    wtf

--- flake.nix
+++ flake.nix
@@ -1,14 +1,13 @@
 {
-  inputs = { nixpkgs.url = "github:nixos/nixpkgs" ; flake-utils.url = "github:numtide/flake-utils" ; } ;
+  inputs = { flake-utils.url = "github:numtide/flake-utils" ; } ;
   outputs =
-    { self , nixpkgs , flake-utils } :
+    { self , flake-utils } :
       flake-utils.lib.eachDefaultSystem
       (
         system :
           let
-            pkgs = builtins.getAttr system nixpkgs.legacyPackages ;
             in
-	      script :
+	      pkgs : script :
 		{
 		  devShell =
 		    pkgs.mkShell
